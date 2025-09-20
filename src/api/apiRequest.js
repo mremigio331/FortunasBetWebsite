254d9205e1917e65eec402a47233db0a61e511f3
MattRemy <m.remigio331@gmail.com>
2025-09-20 15:15:41 -0700

    Adding the git hash to each request to track which version of the website the user is using.

--- src/api/apiRequest.js
+++ src/api/apiRequest.js
@@ -1,41 +1,46 @@
 import axios from "axios";
+import { commitHash as gitCommitHash } from "../constants/gitHash";
+
+const getHeaders = (idToken) => {
+  const headers = {
+    "Content-Type": "application/json",
+    ...(idToken && { Authorization: `Bearer ${idToken}` }),
+  };
+
+  if (gitCommitHash) {
+    headers["X-Git-Commit"] = gitCommitHash;
+  } else {
+    console.warn(
+      "X-Git-Commit header not added because the commit hash is empty.",
+    );
+  }
+
+  return headers;
+};
 
 export const apiRequestGet = (apiEndpoint, route, idToken) => {
   return axios.get(encodeURI(`${apiEndpoint}${route}`), {
-    // Remove withCredentials for GET to avoid preflight CORS issues
-    headers: {
-      "Content-Type": "application/json",
-      ...(idToken && { Authorization: `Bearer ${idToken}` }),
-    },
+    headers: getHeaders(idToken),
   });
 };
 
 export const apiRequestPost = ({ apiEndpoint, idToken, body }) => {
   return axios.post(apiEndpoint, body, {
     withCredentials: true,
-    headers: {
-      "Content-Type": "application/json",
-      ...(idToken && { Authorization: `Bearer ${idToken}` }),
-    },
+    headers: getHeaders(idToken),
   });
 };
 
 export const apiRequestPut = ({ apiEndpoint, idToken, body }) => {
   return axios.put(apiEndpoint, body, {
     withCredentials: true,
-    headers: {
-      "Content-Type": "application/json",
-      ...(idToken && { Authorization: `Bearer ${idToken}` }),
-    },
+    headers: getHeaders(idToken),
   });
 };
 
 export const apiRequestDelete = ({ apiEndpoint, idToken }) => {
   return axios.delete(apiEndpoint, {
     withCredentials: true,
-    headers: {
-      "Content-Type": "application/json",
-      ...(idToken && { Authorization: `Bearer ${idToken}` }),
-    },
+    headers: getHeaders(idToken),
   });
 };
